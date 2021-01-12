8d46773c1e5e06f8bf886bdaaca74aeb96c1dd57
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-11 17:19:07 -0800

    Authentication route

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import Navbar from "./utility/Navbar/Navbar";
 import Footer from "./utility/Footer/Footer";
-import Signup from "./pages/Signup/Signup";
+import Signin from "./pages/Signin/Signin";
+import Protected from "./pages/protected/protect";
+import Activate from "./pages/Signup/Activate";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
@@ -11,7 +14,9 @@ function App() {
         <Route path='/' component={Navbar} />
         <Switch>
           <Route exact path='/' component={Home} />
-          {/* <Route exact path='/signup' component={Signup} /> */}
+          <Route exact path='/signin' component={Signin} />
+          <Route exact path='/protected' component={Protected} />
+          <Route exact path='/auth/activate/:token' component={Activate} />
         </Switch>
         <Route path='/' component={Footer} />
       </div>
