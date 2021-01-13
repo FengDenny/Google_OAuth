e9c2436922e24e0dc623895b17d879f7d627c3fe
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-12 19:39:09 -0800

    Private/Admin route

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import Home from "./pages/Home/Home";
 import Navbar from "./utility/Navbar/Navbar";
 import Footer from "./utility/Footer/Footer";
 import Signin from "./pages/Signin/Signin";
-import Protected from "./pages/protected/protect";
+import Protected from "./pages/Protected/Protect";
+import Admin from "./pages/Admin/Admin";
 import Activate from "./pages/Signup/Activate";
-
+// to create a private and protected routing
+import PrivateRoute from "./pages/Protected/PrivateRoute";
+import AdminRoute from "./pages/Admin/AdminRoute";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
@@ -15,8 +18,10 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/signin' component={Signin} />
-          <Route exact path='/protected' component={Protected} />
           <Route exact path='/auth/activate/:token' component={Activate} />
+          {/* Private/protected */}
+          <PrivateRoute exact path='/protected' component={Protected} />
+          <AdminRoute exact path='/protected-admin' component={Admin} />
         </Switch>
         <Route path='/' component={Footer} />
       </div>
