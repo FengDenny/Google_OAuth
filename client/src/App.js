bd8d9b27d0f72a262bdb7608285060e3c295515b
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-14 20:26:45 -0800

    Forgot/Reset route

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,12 @@ import Signin from "./pages/Signin/Signin";
 import Protected from "./pages/Protected/Protect";
 import Admin from "./pages/Admin/Admin";
 import Activate from "./pages/Signup/Activate";
+import Forgot from "./pages/ForgotPassword/ForgotPassword";
+import Reset from "./pages/ResetPassword/ResetPassword";
 // to create a private and protected routing
 import PrivateRoute from "./pages/Protected/PrivateRoute";
 import AdminRoute from "./pages/Admin/AdminRoute";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
@@ -19,6 +22,8 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/signin' component={Signin} />
           <Route exact path='/auth/activate/:token' component={Activate} />
+          <Route exact path='/auth/password/forgot' component={Forgot} />
+          <Route exact path='/auth/password/reset/:token' component={Reset} />
           {/* Private/protected */}
           <PrivateRoute exact path='/protected' component={Protected} />
           <AdminRoute exact path='/protected-admin' component={Admin} />
