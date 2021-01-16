3aa83b9b97f4824c5cd84209126a9e3583abebbe
Denny Feng <dfeng1@mail.sfsu.edu>
2021-01-15 17:54:03 -0800

    react-facebook-login is now fixed

--- client/src/pages/Facebook/Facebook.js
+++ client/src/pages/Facebook/Facebook.js
@@ -1,15 +1,15 @@
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import FacebookLogin from "react-facebook-login";
 import React from "react";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
-library.add(faFacebook);
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// library.add(faFacebook);
 
 function Facebook({ informParent = (i) => i }) {
   const responseFacebook = (res) => {
     console.log(res);
-    const url = `${process.env.REACT_APP_API}/facebook-login`;
+    const url = `${process.env.REACT_APP_API}/facebok-login`;
     const data = { userID: res.userID, accessToken: res.accessToken };
     axios
       .post(url, data)
@@ -29,17 +29,8 @@ function Facebook({ informParent = (i) => i }) {
         appId={`${process.env.REACT_APP_FACEBOOK_APP_ID}`}
         autoLoad={false}
         callback={responseFacebook}
-        render={(renderProps) => (
-          <div className='form-group'>
-            <button
-              onClick={renderProps.onClick}
-              className='btn-primary btn-primary-facebook xsm'
-            >
-              <FontAwesomeIcon icon={faFacebook} className='facebook-icon' />
-              <span className='facebook-span'>Login with Facebook</span>
-            </button>
-          </div>
-        )}
+        cssClass=' width btn-primary btn-primary-facebook xsm '
+        icon='fa-facebook'
       />
     </div>
   );
